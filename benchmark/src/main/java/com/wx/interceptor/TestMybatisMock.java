7f37aae2053d5616dc9201a648dbe862944b38ba
WUXIN <myeagleandsnake@gmail.com>
2025-04-25 14:40:26 +0800

    feat: 基准测试环境搭建

---
+++ benchmark/src/main/java/com/wx/interceptor/TestMybatisMock.java
@@ -0,0 +1,40 @@
+package com.wx.interceptor;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+
+/**
+ * @author wuxin
+ * @date 2025/04/25 12:19:51
+ */
+public class TestMybatisMock {
+    private static SqlSessionFactory sqlSessionFactory;
+
+    public static void main(String[] args) {
+        try (Reader reader = Resources.getResourceAsReader("mybatis-config-native.xml")) {
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+            sqlSessionFactory.getConfiguration().setLogPrefix("mybatis.logger");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+            UserDao mapper = sqlSession.getMapper(UserDao.class);
+            String s = mapper.selectNameAndIds("hello", new ArrayList<Long>() {{
+                add(1L);
+                add(1L);
+            }});
+            System.out.println(s);
+        }
+
+
+    }
+
+
+}
